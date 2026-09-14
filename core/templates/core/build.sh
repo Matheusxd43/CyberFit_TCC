994fa74146cccba0d22e2a64dabd323847366aec
Matheus Rodrigues <matheusdanonexd23@gmail.com>
2026-09-13 22:10:09 -0300

    fix: altera senha para teste

--- core/templates/core/build.sh
+++ core/templates/core/build.sh
@@ -12,4 +12,5 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # Cria o superusuário automaticamente se ele não existir
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); user, created = User.objects.get_or_create(username='matheusalmeida', defaults={'email': 'admin@example.com'}); user.set_password('200303'); user.is_superuser = True; user.is_staff = True; user.save()"
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); user, created = User.objects.get_or_create(username='matheusalmeida', defaults={'email': 'admin@example.com'}); user.set_password('123456'); user.is_superuser = True; user.is_staff = True; user.save()"
+
