9e95c5a48eb100a9a1bc0cc94b19c9428bfb068b
Matheus Rodrigues <matheusdanonexd23@gmail.com>
2026-09-13 23:15:06 -0300

    fix: ajusta script do build para criar admin com seguranca

--- build.sh
+++ build.sh
@@ -1,17 +1,9 @@
 #!/usr/bin/env bash
-# Exit on error
 set -o errexit
 
-# Instala as dependências
 pip install -r requirements.txt
-
-# Coleta os arquivos estáticos (CSS, imagens do robô, etc.)
 python manage.py collectstatic --no-input
-
-# Prepara o banco de dados
 python manage.py migrate
 
-# Cria o superusuário automaticamente se ele não existir
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); user, created = User.objects.get_or_create(username='matheusalmeida'); user.set_password('123456'); user.is_superuser = True; user.is_staff = True; user.save(); print('SUCESSO: Usuario atualizado')"
-
-
+# Cria o superusuário de forma segura sem quebrar se ele já existir
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='matheusalmeida').exists() or User.objects.create_superuser('matheusalmeida', 'admin@example.com', '123456')"
