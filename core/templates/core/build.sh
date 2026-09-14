105568b2ab24b52f3d419472e81ac9c195a0ad5c
Matheus Rodrigues <matheusdanonexd23@gmail.com>
2026-09-13 22:05:32 -0300

    fix: atualiza senha do superusuario de forma garantida

--- core/templates/core/build.sh
+++ core/templates/core/build.sh
@@ -12,4 +12,4 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # Cria o superusuário automaticamente se ele não existir
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='matheusalmeida').exists() or User.objects.create_superuser('matheusalmeida', 'admin@example.com', '200303')"
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); user, created = User.objects.get_or_create(username='matheusalmeida', defaults={'email': 'admin@example.com'}); user.set_password('200303'); user.is_superuser = True; user.is_staff = True; user.save()"
