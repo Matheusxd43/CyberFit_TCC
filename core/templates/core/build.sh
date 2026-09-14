5217b73ae4ce98e1d73357c46f9bed3dab24510c
Matheus Rodrigues <matheusdanonexd23@gmail.com>
2026-09-13 22:51:01 -0300

    fix: atualiza senha do admin de forma garantida

--- core/templates/core/build.sh
+++ core/templates/core/build.sh
@@ -12,5 +12,6 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # Cria o superusuário automaticamente se ele não existir
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); user, created = User.objects.get_or_create(username='matheusalmeida', defaults={'email': 'admin@example.com'}); user.set_password('123456'); user.is_superuser = True; user.is_staff = True; user.save()"
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); user, created = User.objects.get_or_create(username='matheusalmeida'); user.set_password('123456'); user.is_superuser = True; user.is_staff = True; user.save(); print('SUCESSO: Usuario atualizado')"
+
 
